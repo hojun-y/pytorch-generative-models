01619af2f938e57965af95c8598bbf0bc2d8021d
Hojun Yoon <hojun_yoon@korea.edu>
2023-01-15 04:40:11 +0900

    ⚙️ Implement swiss roll dataset generator 🌀

---
+++ diffusion/swiss_roll.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+
+
+class SwissRollData:
+    def __init__(self, m=1.):
+        self.twopi = torch.pi * 2
+        self.scale = m / self.twopi
+        self.m = m
+
+        viz_pts = np.linspace(0, self.twopi, 100)
+        self.viz_x = self.scale * viz_pts * np.sin(viz_pts)
+        self.viz_y = self.scale * viz_pts * np.cos(viz_pts)
+
+    def get_data(self, batch_size, device='cuda'):
+        pos = torch.rand(batch_size, device=device, requires_grad=False) * self.twopi
+        batch = torch.stack([torch.sin(pos), torch.cos(pos)]) * pos
+        batch *= self.scale
+        return batch.view(-1, 2)
+
+    def viz_data(self, x, show=True):
+        with torch.no_grad():
+            x = x.view(2, -1).cpu().numpy()
+            plt.plot(self.viz_x, self.viz_y, 'k')
+            plt.xlim([-self.m - .1, self.m + .1])
+            plt.ylim([-self.m - .1, self.m + .1])
+            plt.scatter(x[0], x[1], c='r', marker='.')
+            if show:
+                plt.show()

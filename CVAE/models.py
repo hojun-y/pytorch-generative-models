260b83b4ba16ec155b27c33b73c47ce04eb528b4
Hojun Yoon <hojun_yoon@korea.edu>
2023-01-16 02:18:04 +0900

    🗑 Remove redundant codes - class `EncoderLinear` - class `DecoderLinear` These models were made for testing the training loop and will not be compatible with the current implementation.

--- CVAE/models.py
+++ CVAE/models.py
@@ -1,44 +1,5 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
-
-
-class EncoderLinear(nn.Module):
-    def __init__(self, params):
-        super().__init__()
-        self.params = params
-        self.main_net = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(784, params['n'] * 2),
-            nn.ELU(inplace=True),
-            nn.Linear(params['n'] * 2, params['n'] * 1),
-            nn.ELU(inplace=True),
-        )
-        self.var = nn.Linear(params['n'], params['z'])
-        self.mean = nn.Linear(params['n'], params['z'])
-
-    def forward(self, x):
-        main_out = self.main_net(x)
-        var = F.relu(self.var(main_out), True)
-        mean = self.mean(main_out)
-        return var, mean
-
-
-class DecoderLinear(nn.Module):
-    def __init__(self, params):
-        super().__init__()
-        self.params = params
-        self.net = nn.Sequential(
-            nn.Linear(params['z'], params['n']),
-            nn.ELU(inplace=True),
-            nn.Linear(params['n'], params['n'] * 2),
-            nn.ELU(inplace=True),
-            nn.Linear(params['n'] * 2, 784),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, z):
-        return self.net(z)
 
 
 class EncoderConv(nn.Module):
